6e8557e550da66a2323ccf0be595e734f7c101ea
Orange_Peel <citrus.shell@gmail.com>
2024-02-10 22:07:17 -0800

    aauuhh

---
+++ src/main/java/orangepeel/orangeadditions/BlockCrystal.java
@@ -0,0 +1,133 @@
+package orangepeel.orangeadditions;
+
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.entity.TileEntity;
+import net.minecraft.core.block.material.Material;
+import net.minecraft.core.entity.EntityLiving;
+import net.minecraft.core.enums.EnumDropCause;
+import net.minecraft.core.util.helper.Side;
+import net.minecraft.core.util.phys.AABB;
+import net.minecraft.core.world.World;
+
+public class BlockCrystal extends Block {
+
+
+	public BlockCrystal(String key, int id, Material material) {
+		super(key, id, material);
+		float f = 0.2F;
+		this.setBlockBounds(0.5F - f, 0.0F, 0.5F - f, 0.5F + f, f * 3.0F, 0.5F + f);
+	}
+	public AABB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {
+		return null;
+	}
+
+	public boolean isOpaqueCube() {
+		return false;
+	}
+
+	public boolean renderAsNormalBlock() {
+		return false;
+	}
+	public boolean canPlaceBlockAt(World world, int x, int y, int z) {
+		if (world.isBlockNormalCube(x - 1, y, z)) {
+			return true;
+		} else if (world.isBlockNormalCube(x + 1, y, z)) {
+			return true;
+		} else if (world.isBlockNormalCube(x, y, z - 1)) {
+			return true;
+		} else if (world.isBlockNormalCube(x, y + 1, z )) {
+			return true;
+		}
+		else {
+			return world.isBlockNormalCube(x, y, z + 1) ? true : world.canPlaceOnSurfaceOfBlock(x, y - 1, z);
+		}
+	}
+	public void onBlockPlaced(World world, int x, int y, int z, Side side, EntityLiving entity, double sideHeight) {
+		int l = side.getId();
+		int i1 = world.getBlockMetadata(x, y, z);
+		if (l == 1 && world.isBlockNormalCube(x, y + 1, z)) {
+			i1 = 6;
+		}
+		if (l == 1 && world.isBlockNormalCube(x, y - 1, z)) {
+			i1 = 5;
+		}
+
+		if (l == 2 && world.isBlockNormalCube(x, y, z + 1)) {
+			i1 = 4;
+		}
+
+		if (l == 3 && world.isBlockNormalCube(x, y, z - 1)) {
+			i1 = 3;
+		}
+
+		if (l == 4 && world.isBlockNormalCube(x + 1, y, z)) {
+			i1 = 2;
+		}
+
+		if (l == 5 && world.isBlockNormalCube(x - 1, y, z)) {
+			i1 = 1;
+		}
+
+		world.setBlockMetadataWithNotify(x, y, z, i1);
+	}
+	public void onNeighborBlockChange(World world, int x, int y, int z, int blockId) {
+		if (this.dropCrystalIfCantStay(world, x, y, z)) {
+			int i1 = world.getBlockMetadata(x, y, z);
+			boolean flag = false;
+			if (!world.isBlockNormalCube(x - 1, y, z) && i1 == 1) {
+				flag = true;
+			}
+
+			if (!world.isBlockNormalCube(x + 1, y, z) && i1 == 2) {
+				flag = true;
+			}
+
+			if (!world.isBlockNormalCube(x, y, z - 1) && i1 == 3) {
+				flag = true;
+			}
+
+			if (!world.isBlockNormalCube(x, y, z + 1) && i1 == 4) {
+				flag = true;
+			}
+
+			if (!world.isBlockNormalCube(x, y - 1, z) && i1 == 5) {
+				flag = true;
+			}
+			if (!world.isBlockNormalCube(x, y + 1, z) && i1 == 6) {
+				flag = true;
+			}
+
+			if (flag) {
+				this.dropBlockWithCause(world, EnumDropCause.WORLD, x, y, z, i1, (TileEntity) null);
+				world.setBlockWithNotify(x, y, z, 0);
+			}
+		}
+	}
+	public void onBlockAdded(World world, int i, int j, int k) {
+		if (world.isBlockNormalCube(i - 1, j, k)) {
+			world.setBlockMetadataWithNotify(i, j, k, 1);
+		} else if (world.isBlockNormalCube(i + 1, j, k)) {
+			world.setBlockMetadataWithNotify(i, j, k, 2);
+		} else if (world.isBlockNormalCube(i, j, k - 1)) {
+			world.setBlockMetadataWithNotify(i, j, k, 3);
+		} else if (world.isBlockNormalCube(i, j, k + 1)) {
+			world.setBlockMetadataWithNotify(i, j, k, 4);
+		} else if (world.isBlockNormalCube(i, j - 1, k)) {
+			world.setBlockMetadataWithNotify(i, j, k, 5);
+		} else if (world.isBlockNormalCube(i, j + 1, k)) {
+			world.setBlockMetadataWithNotify(i, j, k, 6);
+		}
+
+		this.dropCrystalIfCantStay(world, i, j, k);
+	}
+	private boolean dropCrystalIfCantStay(World world, int i, int j, int k) {
+		if (!this.canPlaceBlockAt(world, i, j, k)) {
+			this.dropBlockWithCause(world, EnumDropCause.WORLD, i, j, k, world.getBlockMetadata(i, j, k), (TileEntity)null);
+			world.setBlockWithNotify(i, j, k, 0);
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+}
---
+++ src/main/java/orangepeel/orangeadditions/OrangeAdditions.java
@@ -0,0 +1,124 @@
+package orangepeel.orangeadditions;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.render.block.model.BlockModelRenderBlocks;
+import net.minecraft.client.sound.block.BlockSound;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockMushroom;
+import net.minecraft.core.block.material.Material;
+import net.minecraft.core.block.tag.BlockTags;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.item.material.ArmorMaterial;
+import net.minecraft.core.item.ItemArmor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.helper.*;
+
+
+public class OrangeAdditions implements ModInitializer {
+    public static final String MOD_ID = "orangeadditions";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	//armor
+	public static final ArmorMaterial speedBoosterMaterial = ArmorHelper.createArmorMaterial(
+		"testarmor",
+		300,
+		20,
+		20,
+		0,
+		50
+	);
+	//item ting :3
+	public static final Item brightitePowder = ItemHelper.createItem(OrangeAdditions.MOD_ID,
+		new Item("brightitepowder", 20000),
+		"brightitepowder",
+		"brightitepowder.png");
+	public static final Item blightiteShards = ItemHelper.createItem(OrangeAdditions.MOD_ID,
+		new Item("blightiteshards", 20001),
+		"blightiteshards",
+		"blightiteshards.png");
+	public static final Item prismariteSnow = ItemHelper.createItem(OrangeAdditions.MOD_ID,
+		new Item("prismaritesnow", 20002),
+		"prismaritesnow",
+		"prismaritesnow.png");
+	public static final Item pixieDust = ItemHelper.createItem(OrangeAdditions.MOD_ID,
+		new Item("pixiedust", 20003),
+		"pixiedust",
+		"pixiedust.png");
+	public static final Item artificialWing = ItemHelper.createItem(OrangeAdditions.MOD_ID,
+		new Item("artificialwing", 20004),
+		"artificialwing",
+		"artificialwing.png");
+	public static final ItemArmor speedBoosters = (ItemArmor) ItemHelper.createItem(OrangeAdditions.MOD_ID,
+		new ItemArmor("speedboosters", 20005, speedBoosterMaterial, 2),
+		"speedboosters",
+		"speedboosters.png");
+
+
+	//blocks
+	public static final Block brightiteCrystal = new BlockBuilder(MOD_ID)
+		.setBlockSound(new BlockSound("step.stone", "random.glass", 1.0f, 1.0f))
+		.setHardness (0.0f)
+		.setResistance(0.0f)
+		.setLuminance(9)
+		.setTextures("brightite.png")
+		.setBlockModel(new BlockModelRenderBlocks(1))
+		.setTags(BlockTags.BROKEN_BY_FLUIDS)
+		.build(new BlockCrystal("brightitecrystal", 10000, Material.plant));
+	public static final Block blightiteCrystal = new BlockBuilder(MOD_ID)
+		.setBlockSound(new BlockSound("step.stone", "random.glass", 1.0f, 1.0f))
+		.setHardness (0.0f)
+		.setResistance(0.0f)
+		.setTextures("blightite.png")
+		.setBlockModel(new BlockModelRenderBlocks(1))
+		.setTags(BlockTags.BROKEN_BY_FLUIDS)
+		.build(new BlockCrystal("blightitecrystal", 10001, Material.plant));
+	public static final Block prismariteFlake = new BlockBuilder(MOD_ID)
+		.setBlockSound(new BlockSound("step.stone", "random.glass", 1.0f, 1.0f))
+		.setHardness (0.0f)
+		.setResistance(0.0f)
+		.setLuminance(4)
+		.setTextures("prismarite.png")
+		.setBlockModel(new BlockModelRenderBlocks(1))
+		.setTags(BlockTags.BROKEN_BY_FLUIDS)
+		.build(new BlockCrystal("prismariteflake", 10002, Material.plant));
+	public static final Block whiteMushroom = new BlockBuilder(MOD_ID)
+		.setBlockSound(new BlockSound("step.grass", "step.grass", 1.0f, 1.0f))
+		.setHardness(0.0f)
+		.setResistance(0.0f)
+		.setTextures("whitemushroom.png")
+		.setBlockModel(new BlockModelRenderBlocks(1))
+		.setTags(BlockTags.SHEARS_DO_SILK_TOUCH, BlockTags.BROKEN_BY_FLUIDS)
+		.build(new BlockMushroom("whitemushroom", 10003));
+
+	//recipies
+	final static String[] recipeSpeedBoosters = {
+		"AAA",
+		"B B",
+		"C C"
+	};
+
+
+
+    @Override
+    public void onInitialize() {
+
+		LOGGER.info("OrangeAdditions initialized.");
+		//recipies?
+		RecipeHelper.Crafting.createShapelessRecipe(new ItemStack(artificialWing, 1),  new Object[]{
+			Item.leather,
+			Item.featherChicken,
+			pixieDust,
+			pixieDust
+		});
+		RecipeHelper.Crafting.createRecipe(new ItemStack(speedBoosters, 1), new Object[]{
+			recipeSpeedBoosters,
+			'A', Item.ingotSteel,
+			'B', blightiteShards,
+			'C', artificialWing
+		});
+	}
+
+
+}
